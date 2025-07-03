f42704fe765463aa432ad8c9533abccf841339eb
Harish Masi <harishmasi2512@gmail.com>
2025-07-03 09:19:22 +0530

    Updated Version

--- vm-list18.ps1
+++
@@ -1,3 +0,0 @@
-$vmList = @(
-    @{ VMName = "azeune3ccqc01"; MemorySize = "16GB"; ResourceId = "/subscriptions/a11b8ab5-3cf1-4275-8f02-a1895972618b/resourceGroups/SAP-LMP1-CC-EUNE-RSG-CCQ/providers/Microsoft.Compute/virtualMachines/azeune3ccqc01" }
-)
--- vm-list5.ps1
+++
@@ -1,5 +0,0 @@
-$vmList = @(
-    @{ VMName = "azuse23nfda01"; MemorySize = "64GB";   ResourceId = "/subscriptions/dd56ba3d-657d-489f-ae2a-108d2ace3693/resourceGroups/SAP-LLP1-D-USE2-RSG-APPLRS/providers/Microsoft.Compute/virtualMachines/azuse23nfda01" },
-    @{ VMName = "azuse23ld1a01"; MemorySize = "64GB";   ResourceId = "/subscriptions/dd56ba3d-657d-489f-ae2a-108d2ace3693/resourceGroups/SAP-LLP1-D-USE2-RSG-APPLRS/providers/Microsoft.Compute/virtualMachines/azuse23ld1a01" },
-    @{ VMName = "azuse23ld1h01"; MemorySize = "875GB";  ResourceId = "/subscriptions/dd56ba3d-657d-489f-ae2a-108d2ace3693/resourceGroups/sap-llp1-d-use2-rsg-dblrs/providers/Microsoft.Compute/virtualMachines/azuse23ld1h01" }
-)
